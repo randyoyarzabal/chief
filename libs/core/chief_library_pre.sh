12b01ad08cf9f27681695c4b5a5e5034b2d24da6
Randy E. Oyarzabal <royarzab@redhat.com>
2022-01-29 20:50:01 -0600

    Confirm auto-update.

--- libs/core/chief_library_pre.sh
+++ libs/core/chief_library_pre.sh
@@ -24,7 +24,6 @@ CHIEF_PLUGINS_CORE="${CHIEF_PLUGINS}"
 # CORE HELPER FUNCTIONS
 ###################################################################################################################
 
-
 # Detect platform
 uname_out="$(uname -s)"
 case "${uname_out}" in
