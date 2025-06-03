0a38cf7bc128fb8fd14b31809a9b7e5e66d2accc
Randy E. Oyarzabal <github@randyoyarzabal.com>
2025-06-03 17:15:51 -0500

    Help text updates.

--- tools/uninstall.sh
+++ tools/uninstall.sh
@@ -10,6 +10,13 @@ CHIEF_COLOR_GREEN='\033[0;32m'
 CHIEF_COLOR_YELLOW='\033[1;33m'
 CHIEF_NO_COLOR='\033[0m' # Reset color/style
 
+# Chief Environment
+local config_lines=(
+  "export CHIEF_CONFIG=\"\$HOME/.chief_config.sh\""
+  "export CHIEF_PATH=\"\$HOME/.chief\""
+  "source \${CHIEF_PATH}/chief.sh"
+)
+
 function _chief.banner {
   echo -e "${CHIEF_COLOR_YELLOW}        __    _      ____${CHIEF_NO_COLOR}"
   echo -e "${CHIEF_COLOR_YELLOW}  _____/ /_  (_)__  / __/${CHIEF_NO_COLOR}"
@@ -69,11 +76,21 @@ function _chief_uninstall {
   fi
   # Remove lines from .bashrc
   if [[ -f "$HOME/.bashrc" ]]; then
-    echo -e "${CHIEF_COLOR_BLUE}Removing Chief lines from ~/.bashrc...${CHIEF_NO_COLOR}"
-    sed -i '/export CHIEF_CONFIG="\$HOME\/.chief_config.sh"/d' "$HOME/.bashrc"
-    sed -i '/export CHIEF_PATH="\$HOME\/.chief"/d' "$HOME/.bashrc"
-    sed -i '/source \${CHIEF_PATH}\/chief.sh/d' "$HOME/.bashrc"
-    echo -e "${CHIEF_COLOR_GREEN}Chief lines removed from ~/.bashrc.${CHIEF_NO_COLOR}"
+    if [[ $(uname) == "Darwin" ]]; then
+      # TODO: Detect if gnu sed is installed and use that, for now, instruct user to do the removal manually
+      echo -e "${CHIEF_COLOR_YELLOW}Remove the following lines manually from ~/.bashrc${CHIEF_NO_COLOR}:"
+      for line in "${config_lines[@]}"; do
+        echo -e "${CHIEF_COLOR_YELLOW}$line${CHIEF_NO_COLOR}"
+      done
+    else
+      # Use sed to remove lines from .bashrc on non MacOS systems
+      echo -e "${CHIEF_COLOR_BLUE}Removing Chief lines from ~/.bashrc...${CHIEF_NO_COLOR}"
+      for line in "${config_lines[@]}"; do
+        echo -e "${CHIEF_COLOR_BLUE}Removing line from ~/.bashrc: $line${CHIEF_NO_COLOR}"
+        sed -i "/$(echo "$line" | sed 's/[\/&]/\\&/g')/d" "$HOME/.bashrc"
+      done
+      echo -e "${CHIEF_COLOR_GREEN}Chief lines removed from ~/.bashrc.${CHIEF_NO_COLOR}"
+    fi
   else
     echo -e "${CHIEF_COLOR_YELLOW}~/.bashrc does not exist, nothing to remove.${CHIEF_NO_COLOR}"
   fi 
