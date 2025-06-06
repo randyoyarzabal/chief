06dc32817784b827723baed6d234b2720e5f1edd
Randy E. Oyarzabal <github@randyoyarzabal.com>
2025-06-06 16:25:36 -0500

    Added license header.

--- chief.sh
+++ chief.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+# Copyright (C) 2025 Randy E. Oyarzabal <github@randyoyarzabal.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ###################################################################################################################
 # WARNING: This file is not meant to be edited/configured directly unless you know what you are doing.
 #   All settings and commands are done via the chief.* commands
--- libs/core/plugins/git_chief-plugin.sh
+++ libs/core/plugins/git_chief-plugin.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+# Copyright (C) 2025 Randy E. Oyarzabal <github@randyoyarzabal.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # Chief Plugin File: git_chief-plugin.sh
 # Author: Randy E. Oyarzabal
