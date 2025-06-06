0c0a0911a10d7fb0a02aeca422a7be6ef95ad3d2
Randy E. Oyarzabal <github@randyoyarzabal.com>
2025-06-06 16:15:10 -0500

    Fixed install/uninstall scripts.

--- tools/uninstall.sh
+++ tools/uninstall.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env bash
 
+# $1 sets CHIEF_PATH, $2 sets CHIEF_CONFIG
+
+# Set default .bashrc vars
 CHIEF_PATH="$HOME/.chief"
-CHIEF_CONFIG="$HOME/.chief_config.sh" 
-BASHRC_FILE="$HOME/.bashrc"
+CHIEF_CONFIG="$HOME/.chief_config.sh"
+
+# Check for any overrides from defaults
+if [[ -n "$1" ]]; then
+  CHIEF_PATH="$2"
+fi
+
+if [[ -n "$2" ]]; then
+  CHIEF_CONFIG="$3"
+fi
+
+# Chief loading lines for .bashrc
+CHIEF_CONFIG_LINES=(
+  "export CHIEF_CONFIG=$CHIEF_CONFIG"
+  "export CHIEF_PATH=$CHIEF_PATH"
+  "source $CHIEF_PATH/chief.sh"
+)
+
+CHIEF_BASHRC="$HOME/.bashrc"
 
 CHIEF_COLOR_RED='\033[0;31m'
 CHIEF_COLOR_BLUE='\033[0;34m'
@@ -10,13 +30,6 @@ CHIEF_COLOR_CYAN='\033[0;36m'
 CHIEF_COLOR_GREEN='\033[0;32m'
 CHIEF_COLOR_YELLOW='\033[1;33m'
 CHIEF_NO_COLOR='\033[0m' # Reset color/style
-
-# Chief Environment
-CHIEF_CONFIG_LINES=(
-  "export CHIEF_CONFIG=\"\$HOME/.chief_config.sh\""
-  "export CHIEF_PATH=\"\$HOME/.chief\""
-  "source \${CHIEF_PATH}/chief.sh"
-)
 
 function _chief.banner {
   echo -e "${CHIEF_COLOR_YELLOW}        __    _      ____${CHIEF_NO_COLOR}"
@@ -49,7 +62,20 @@ Example:
 }
 
 function _chief_uninstall {
+  if [[ ! -d $CHIEF_PATH ]]; then
+  echo "Uninstallation parameters:
+  CHIEF_PATH=$CHIEF_PATH
+  CHIEF_CONFIG=$CHIEF_CONFIG"
+  
+    echo -e "${CHIEF_COLOR_YELLOW}Chief is not installed, nothing to remove.${CHIEF_NO_COLOR}"
+    return 1
+  fi
+
+  # Check if Chief is installed 
   response=$(_chief_confirm "Are you sure you want to uninstall the Chief utility? 
+Uninstallation parameters:
+  CHIEF_PATH=$CHIEF_PATH
+  CHIEF_CONFIG=$CHIEF_CONFIG
 The configuration file will be backed up as ${CHIEF_CONFIG}.backup.
 All plugin files and plugin directories will NOT be removed.
 This action cannot be undone. Proceed with uninstallation?")
@@ -58,16 +84,13 @@ This action cannot be undone. Proceed with uninstallation?")
     return 0
   fi
   # Remove the Chief installation directory
-  if [[ -d $CHIEF_PATH ]]; then
-    echo -e "${CHIEF_COLOR_BLUE}Removing Chief installation directory...${CHIEF_NO_COLOR}"
-    rm -rf "$CHIEF_PATH" || {
-      echo -e "${CHIEF_COLOR_RED}Error: Could not remove Chief installation directory.${CHIEF_NO_COLOR}"
-      return 1
-    }
-    echo -e "${CHIEF_COLOR_GREEN}Chief installation directory removed successfully.${CHIEF_NO_COLOR}"
-  else
-    echo -e "${CHIEF_COLOR_YELLOW}Chief is not installed, nothing to remove.${CHIEF_NO_COLOR}"
-  fi
+  echo -e "${CHIEF_COLOR_BLUE}Removing Chief installation directory...${CHIEF_NO_COLOR}"
+  rm -rf "$CHIEF_PATH" || {
+    echo -e "${CHIEF_COLOR_RED}Error: Could not remove Chief installation directory.${CHIEF_NO_COLOR}"
+    return 1
+  }
+  echo -e "${CHIEF_COLOR_GREEN}Chief installation directory removed successfully.${CHIEF_NO_COLOR}"
+
   # Remove the Chief configuration file
   if [[ -f $CHIEF_CONFIG ]]; then
     # Backup the configuration file before removing it
@@ -85,22 +108,14 @@ This action cannot be undone. Proceed with uninstallation?")
   else
     echo -e "${CHIEF_COLOR_YELLOW}Chief configuration file does not exist, nothing to remove.${CHIEF_NO_COLOR}"
   fi
+
   # Remove lines from .bashrc
   if [[ -f "$HOME/.bashrc" ]]; then
-    # if [[ $(uname) == "Darwin" ]]; then
-    #   # TODO: Detect if gnu sed is installed and use that, for now, instruct user to do the removal manually
-    #   echo -e "${CHIEF_COLOR_YELLOW}Remove the following lines manually from ~/.bashrc (if exists).${CHIEF_NO_COLOR}:"
-    #   for line in "${CHIEF_CONFIG_LINES[@]}"; do
-    #     echo -e "  $line"
-    #   done
-    # else
-    # Use sed to remove lines from .bashrc on non MacOS systems
     echo -e "${CHIEF_COLOR_BLUE}Removing Chief lines from ~/.bashrc...${CHIEF_NO_COLOR}"
     for line in "${CHIEF_CONFIG_LINES[@]}"; do
       echo -e "${CHIEF_COLOR_BLUE}Removing line from ~/.bashrc: $line${CHIEF_NO_COLOR}"
-      # sed -i "/$(echo "$line" | sed 's/[\/&]/\\&/g')/d" "$HOME/.bashrc"
       # Portable sed usage; reference: https://unix.stackexchange.com/a/381201
-      sed -i.bak -e "/$(echo "$line" | sed 's/[\/&]/\\&/g')/d" -- "${BASHRC_FILE}" && rm -- "${BASHRC_FILE}.bak"
+      sed -i.bak -e "/$(echo "$line" | sed 's/[\/&]/\\&/g')/d" -- "${CHIEF_BASHRC}" && rm -- "${CHIEF_BASHRC}.bak"
     done
     echo -e "${CHIEF_COLOR_GREEN}Chief lines removed from ~/.bashrc.${CHIEF_NO_COLOR}"
     # fi
