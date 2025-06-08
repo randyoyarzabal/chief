64c11fbc8690a40a40798d947a8aab0322ef8369
Randy E. Oyarzabal <github@randyoyarzabal.com>
2025-06-08 10:16:50 -0500

    Updated page to enable copy button.

--- libs/core/chief_library.sh
+++ libs/core/chief_library.sh
@@ -482,6 +482,7 @@ function __chief.hints_text() {
     echo -e "- ${CHIEF_COLOR_GREEN}chief.plugins${CHIEF_NO_COLOR} to cd into your plugins directory.${CHIEF_NO_COLOR}"
     echo -e "- ${CHIEF_COLOR_GREEN}chief.plugin -?${CHIEF_NO_COLOR} to list plugins.${CHIEF_NO_COLOR}"
     echo -e "- ${CHIEF_COLOR_GREEN}chief.whereis <function | alias>${CHIEF_NO_COLOR} to locate exact location of function or alias.${CHIEF_NO_COLOR}"
+    echo ""
     echo -e "${CHIEF_COLOR_CYAN}** Set ${CHIEF_COLOR_GREEN}CHIEF_CFG_HINTS=false${CHIEF_COLOR_CYAN} to disable these hints. **${CHIEF_NO_COLOR}"
     echo ""
   else
